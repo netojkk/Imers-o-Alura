e4a13f6e338811f0e8318de0a37b7b4ece6c0343
Neto <136371812+netojkk@users.noreply.github.com>
2024-09-05 13:27:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# imersao-alura-javascript
+ Nesse projeto eu estou trabalhando e reforçando alguns conhecimentos em Javascript, criação de listas referente ao tema que eu escolhi e autoamtizando a criação de descrições. Além de criar um sistema de pesquisa.
---
+++ dados.js
@@ -0,0 +1,7 @@
+let filmes = [
+    {
+        titulo : "Como treinar seu Dragão",
+        descricao: "A história se passa na ilha de Berk, onde vikings e dragões travam uma batalha épica. No entanto, Hiccup, um jovem viking diferente dos outros, descobre que a amizade com os dragões é possível.",
+        diretor: "Dean DeBlois"
+    }
+]
