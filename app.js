3d5379df054a3f226af63a08dd29fa9d956cdeb9
Neto <136371812+netojkk@users.noreply.github.com>
2024-09-05 20:27:32 -0300

    adicionando a base de dados e adicionando o Html de maneira dinâmica

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("olá mundo")
+let section = document.getElementById("resultados-pesquisa")
+console.log(section)
+
+section.innerHTML = `
+    <div class="item-resultado">
+        <h2> ${filmes[0].titulo} </h2>
+    
+        <p class="descricao-meta">${filmes[0].descricao} 
+        </p> 
+                
+        <p>
+            Diretor: ${filmes[0].diretor}
+        </p>
+        <p>
+            Ano de Lançamento: 2010
+        </p>
+    </div>
+`
--- dados.js
+++ dados.js
@@ -1,7 +1,44 @@
 let filmes = [
     {
-        titulo : "Como treinar seu Dragão",
+        titulo: "Como treinar seu Dragão",
         descricao: "A história se passa na ilha de Berk, onde vikings e dragões travam uma batalha épica. No entanto, Hiccup, um jovem viking diferente dos outros, descobre que a amizade com os dragões é possível.",
-        diretor: "Dean DeBlois"
-    }
-]
+        diretor: "Dean DeBlois",
+        anoLancamento: 2010
+    },
+    {
+        titulo: "Toy Story",
+        descricao: "Uma aventura emocionante que se passa no mundo dos brinquedos, onde um grupo de amigos vivem grandes aventuras quando seus donos não estão por perto.",
+        diretor: "John Lasseter",
+        anoLancamento: 1995
+    },
+    {
+        titulo: "A Bela e a Fera",
+        descricao: "Uma clássica história de amor entre uma bela jovem e uma fera encantada, que quebra a maldição através do amor verdadeiro.",
+        diretor: "Gary Trousdale e Kirk Wise",
+        anoLancamento: 1991
+    },
+    {
+        titulo: "Moana",
+        descricao: "Uma jovem aventureira que embarca em uma jornada pelo oceano para salvar seu povo e encontrar seu próprio destino.",
+        diretor: "Ron Clements e John Musker",
+        anoLancamento: 2016
+    },
+    {
+        titulo: "Frozen",
+        descricao: "As irmãs Elsa e Anna embarcam em uma jornada épica para encontrar Elsa e quebrar uma maldição que congelou seu reino.",
+        diretor: "Chris Buck e Jennifer Lee",
+        anoLancamento: 2013
+    },
+    {
+        titulo: "Spider-Man: Into the Spider-Verse",
+        descricao: "Miles Morales se torna o Homem-Aranha e une forças com outras versões do herói para salvar o multiverso.",
+        diretor: "Bob Persichetti, Peter Ramsey e Rodney Rothman",
+        anoLancamento: 2018
+    },
+    {
+        titulo: "Shrek",
+        descricao: "Um ogro solitário vive feliz em seu pântano até que sua vida é interrompida pela invasão de diversos personagens de contos de fadas. Para recuperar a paz, Shrek embarca em uma missão para resgatar a Princesa Fiona e devolvê-la a Lord Farquad.",
+        diretor: "Andrew Adamson, Vicky Jenson",
+        anoLancamento: 2001
+      }
+];
